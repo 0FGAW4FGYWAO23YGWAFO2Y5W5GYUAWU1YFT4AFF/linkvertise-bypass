696b0c65bee952bbd9d40cdedcbf14e6fe153a74
0FGAW4FGYWAO23YGWAFO2Y5W5GYUAWU1YFT4AFF <103228398+0FGAW4FGYWAO23YGWAFO2Y5W5GYUAWU1YFT4AFF@users.noreply.github.com>
2022-04-28 22:41:54 -0400

    Add files via upload

---
+++ bypass.py
@@ -0,0 +1,19 @@
+import requests
+import time
+
+def bypass(url):
+  payload = {
+    "url": url,
+  }
+
+  r = requests.post("https://api.bypass.vip/", data=payload)
+  return r.json()
+
+
+if __name__ == '__main__':
+  print('Link: ')
+  result = bypass(input())  # include url to bypass
+  resultvar = repr(result)
+with open('output4awesomeprogram.txt','w',encoding = 'utf-8') as f:
+   f.write(resultvar)
+   print('Check txt for your bypass!')
